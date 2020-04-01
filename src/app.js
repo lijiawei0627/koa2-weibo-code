21fbbbbf1fbd2555261b0590d8c56e670b955ad2
ljw <1472406718@qq.com>
2020-04-01 10:15:16 +0800

    first commit

--- src/app.js
+++ src/app.js
@@ -9,22 +9,24 @@ const logger = require('koa-logger')
 const index = require('./routes/index')
 const users = require('./routes/users')
 
-// error handler
+// 错误监测
 onerror(app)
 
-// middlewares
+// 处理postData数据，可以处理多种格式，最终转成JSON格式
 app.use(bodyparser({
   enableTypes:['json', 'form', 'text']
 }))
 app.use(json())
+// 日志插件，优化打印格式，没有记录日志的功能
 app.use(logger())
+// 将public目录暴露出去，为应用程序提供静态资源
 app.use(require('koa-static')(__dirname + '/public'))
 
 app.use(views(__dirname + '/views', {
   extension: 'ejs'
 }))
 
-// logger
+// 打印当前日志
 app.use(async (ctx, next) => {
   const start = new Date()
   await next()
@@ -32,11 +34,11 @@ app.use(async (ctx, next) => {
   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
 })
 
-// routes
+// 注册路由
 app.use(index.routes(), index.allowedMethods())
 app.use(users.routes(), users.allowedMethods())
 
-// error-handling
+// 错误处理
 app.on('error', (err, ctx) => {
   console.error('server error', err, ctx)
 });
